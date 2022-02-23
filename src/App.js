6a3d2ae6558d18c7f2d92ed8fe268fe5022506af
giana <baldagiana165@gmail.com>
2022-02-23 17:14:00 -0500

    added css and deleted some files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
-import {NewRoot1} from "./NewRoot1";
-    
-export default function App() {
-    return <NewRoot1 />;
+import './App.css';
+import car from './Redcar.png';
+import logo from '.applogo.png';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="AppHeaderBar">
+                <img src={car} className="AppAavator"/>
+                <img src={logo} className="AppLogo"/>
+                   <div className="AppHeaderFont">
+                      <div className="AppLogo">
+                          <div className="AppIcon1">
+                            <div className="AppIcon2">
+                            <div className="AppIcon3">
+
+                         </div>
+                       </div>
+                    </div>
+                  </div>
+               </div>
+            </div>
+        </div>
+        
+    );
 }
-/* Hi my name is giana */ 
+
+export default App;
