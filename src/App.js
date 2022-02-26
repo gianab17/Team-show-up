056c447bc4a01febbf1592be4a0046a370f852a7
giana <baldagiana165@gmail.com>
2022-02-26 14:58:34 -0500

    changes to css
    also added inputs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import logo from './applogo.png';
 import './App.css';
-
+import Form from './components/Form';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App"><Form/>
+      <header className="App-header"></header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing App information
-        </p>
-      </header>
-    </div>
-  );
-}
+
+
+        </div>
+
+        )
+    };
+  
 
 export default App;
---
+++ src/components/SignUpInfo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SignUpInfo() {
+    return (
+        <div className="sign-up-container">
+        <input type="text" placeholder="Type"/>
+        <input type="text" placeholder="model"/>
+        <input type="text" placeholder="mileage"/>
+        <input type="text" placeholder="seats"/>
+        </div>
+
+    ) 
+}
+
+export default SignUpInfo;
