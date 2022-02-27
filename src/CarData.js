b9e7eb99a1d0bd8c40e548f85c5450318cd9a496
602 Industry LLC <jeremylee@602industry.com>
2022-02-26 19:21:07 -0500

    Adjusted JSON Data
    Switched JSON data from file to JS const for sorting.

---
+++ src/CarData.js
@@ -0,0 +1,212 @@
+const Cars = [
+    { 
+        "car_name": "Accord",
+        "car_model": "Honda",
+        "car_type": "Individual",
+        "car_seats": 4,
+        "car_speed": true,
+        "car_gas": "Great",
+        "car_style": true,
+        "car_function": false
+    },
+    { 
+        "car_name": "HRV",
+        "car_model": "Honda",
+        "car_type": "Family/Individual",
+        "car_seats": 5,
+        "car_speed": true,
+        "car_gas": "Good",
+        "car_style": true,
+        "car_function": false
+    },
+    { 
+        "car_name": "Oddessy",
+        "car_model": "Honda",
+        "car_type": "Family",
+        "car_seats": 7,
+        "car_speed": false,
+        "car_gas": "Great",
+        "car_style": false,
+        "car_function": true
+    },
+    { 
+        "car_name": "Focus",
+        "car_model": "Ford",
+        "car_type": "Individual",
+        "car_seats": 4,
+        "car_speed": true,
+        "car_gas": "Great",
+        "car_style": true,
+        "car_function": false
+    },
+    { 
+        "car_name": "F150",
+        "car_model": "Ford",
+        "car_type": "Family/Individual",
+        "car_seats": 6,
+        "car_speed": true,
+        "car_gas": "Great",
+        "car_style": true,
+        "car_function": true
+    },
+    { 
+        "car_name": "Explorer",
+        "car_model": "Ford",
+        "car_type": "Family",
+        "car_seats": 8,
+        "car_speed": false,
+        "car_gas": "Great",
+        "car_style": false,
+        "car_function": true
+    },
+    { 
+        "car_name": "Camry",
+        "car_model": "Toyota",
+        "car_type": "Individual",
+        "car_seats": 4,
+        "car_speed": true,
+        "car_gas": "Great",
+        "car_style": true,
+        "car_function": false
+    },
+    { 
+        "car_name": "Tundra",
+        "car_model": "Toyota",
+        "car_type": "Family/Individual",
+        "car_seats": 6,
+        "car_speed": false,
+        "car_gas": "Great",
+        "car_style": false,
+        "car_function": true
+    },
+    { 
+        "car_name": "Highlander",
+        "car_model": "Toyota",
+        "car_type": "Family",
+        "car_seats": 7,
+        "car_speed": true,
+        "car_gas": "Great",
+        "car_style": true,
+        "car_function": false
+    },
+    { 
+        "car_name": "Pop",
+        "car_model": "Fiat",
+        "car_type": "Individual",
+        "car_seats": 4,
+        "car_speed": true,
+        "car_gas": "Good",
+        "car_style": true,
+        "car_function": false
+    },
+    { 
+        "car_name": "500x",
+        "car_model": "Fiat",
+        "car_type": "Family/Individual",
+        "car_seats": 5,
+        "car_speed": true,
+        "car_gas": "Great",
+        "car_style": true,
+        "car_function": false
+    },
+    { 
+        "car_name": "500L",
+        "car_model": "Fiat",
+        "car_type": "Family",
+        "car_seats": 5,
+        "car_speed": false,
+        "car_gas": "Great",
+        "car_style": true,
+        "car_function": true
+    },
+    { 
+        "car_name": "Civic",
+        "car_model": "Honda",
+        "car_type": "Individual",
+        "car_seats": 4,
+        "car_speed": true,
+        "car_gas": "Great",
+        "car_style": true,
+        "car_function": false
+    },
+    { 
+        "car_name": "Civic",
+        "car_model": "Honda",
+        "car_type": "Individual",
+        "car_seats": 4,
+        "car_speed": true,
+        "car_gas": "Great",
+        "car_style": true,
+        "car_function": false
+    },
+    { 
+        "car_name": "Civic",
+        "car_model": "Honda",
+        "car_type": "Individual",
+        "car_seats": 4,
+        "car_speed": true,
+        "car_gas": "Great",
+        "car_style": true,
+        "car_function": false
+    },
+    { 
+        "car_name": "HRV",
+        "car_model": "Honda",
+        "car_type": "Individual",
+        "car_seats": 5,
+        "car_speed": true,
+        "car_gas": "Great",
+        "car_style": true,
+        "car_function": true
+    },
+    { 
+        "car_name": "Civic",
+        "car_model": "Honda",
+        "car_type": "Individual",
+        "car_seats": 5,
+        "car_speed": true,
+        "car_gas": "Great",
+        "car_style": true,
+        "car_function": false
+    },
+    { 
+        "car_name": "Accord",
+        "car_model": "Honda",
+        "car_type": "Individual",
+        "car_seats": 4,
+        "car_speed": true,
+        "car_gas": "Good",
+        "car_style": true,
+        "car_function": false
+    },
+    { 
+        "car_name": "scion xa",
+        "car_model": "Toyota",
+        "car_type": "Individual",
+        "car_seats": 4,
+        "car_speed": false,
+        "car_gas": "excellent",
+        "car_style": false,
+        "car_function": true 
+    },
+    { 
+        "car_name": "ridgeline",
+        "car_model": "Honda",
+        "car_type": "family/indiviual",
+        "car_seats": 5,
+        "car_speed": true,
+        "car_gas": "Good",
+        "car_style": true,
+        "car_function": false
+    },
+    { 
+        "car_name": "Wrangler",
+        "car_model": "Jeep",
+        "car_type": "Individual",
+        "car_seats": 4,
+        "car_speed": true,
+        "car_gas": "Good",
+        "car_style": true,
+        "car_function": true
+    }      
+    ]
