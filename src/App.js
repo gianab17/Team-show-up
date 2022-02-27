3787449f62ca40e5a5f823c1106f48f47123521e
giana <baldagiana165@gmail.com>
2022-02-27 14:04:04 -0500

    node modules

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App"><Form/>
       <header className="App-header"></header>
         <img src={logo} className="App-logo" alt="logo" />
+        <div clasName="DisplayInfo"></div>
 
 
         </div>
